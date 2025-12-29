a7fde7099365e5bc36f232ee99bbdc21d1a3ab13
V-Jagath <jagath9360@gmail.com>
2025-12-29 15:30:20 +0530

    Updated

--- Back_End/update-phase/src/TASKS/DailyTasks/Day25Task.tsx
+++ Back_End/update-phase/src/TASKS/DailyTasks/Day25Task.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react"
+
 const Day25Task = () => {
+
+    const [data, setData] = useState("")
+
+    const [fdata, setfdata] = useState("")
+
+    const getVal = (e: any) => {
+        e.preventDefault()
+        console.log(e)
+        setData(e.target.value)
+    }
+
+    const formData = (e: any) => {
+        e.preventDefault()
+        setfdata(data)
+        console.log(e.target.value)
+    }
+
     return (
         <>
-
+            <form onClick={formData} >
+                <input type="text" onChange={getVal} className="bg-red-200 text-black" />
+                <input type="submit" value="getData" />
+                <h1>{fdata}</h1>
+            </form>
         </>
     )
 }

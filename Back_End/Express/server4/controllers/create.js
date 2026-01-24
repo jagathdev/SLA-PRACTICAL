adf63b01bea2769c20573bec100b732025faff19
V-Jagath <jagath9360@gmail.com>
2026-01-24 22:13:50 +0530

    Updated

--- Back_End/Express/server4/controllers/create.js
+++ Back_End/Express/server4/controllers/create.js
@@ -1,4 +1,19 @@
 export const create = (req, res) => {
-    console.log(req.body)
-    res.send("Data Added")
+
+    let user = []
+    try {
+        const newUser = {
+            id: user.length + 1,
+            ...req.body,
+            createdBy: req.headers.data || "Not Vaild User"
+        }
+        user.push(newUser)
+
+        res.status(201).json({
+            msg: "User Added",
+            data: newUser
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }

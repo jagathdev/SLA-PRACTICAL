f731702b875cda00ae16bc0c38c833ad140d9d90
V-Jagath <jagath9360@gmail.com>
2026-01-27 19:06:04 +0530

    Task Updated

--- Back_End/React/ailaysa-task/src/App.jsx
+++ Back_End/React/ailaysa-task/src/App.jsx
@@ -1,42 +1,14 @@
 import React, { useState } from 'react'
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  const [input, setInput] = useState({ name: "", age: "", email: "", password: "" })
-  const [formData, setFormData] = useState()
+  return (<>
+    <h1>APP Component</h1>
 
-  const getInput = (e) => {
-    const { name, value } = e.target;
-
-    const saveData = [name, { ...value }]
-
-    console.log(saveData)
-    setInput(saveData)
-
-  }
-
-  const getFormData = (e) => {
-    e.preventDefault()
-    console.log(input)
-  }
-
-  return (
-    <div>
-      <div>
-        <h1>Form Validation</h1>
-        <form onSubmit={getFormData}>
-          <label>Name :</label>
-          <input type="text" onChange={getInput} placeholder='Enter Name' name='name' />
-          <label>Age :</label>
-          <input type="text" onChange={getInput} placeholder='Enter age' name='age' />
-          <label>Email :</label>
-          <input type="email" onChange={getInput} placeholder='Enter email' name='email' />
-          <label>Password :</label>
-          <input type="password" onChange={getInput} placeholder='Enter password' name='password' />
-          <input type="submit" value={"Register"} />
-        </form>
-      </div>
-    </div>
-  )
+    <Routes>
+      <Route path='/' element={Home} />
+    </Routes>
+  </>)
 }
+export default App;
 
-export default App

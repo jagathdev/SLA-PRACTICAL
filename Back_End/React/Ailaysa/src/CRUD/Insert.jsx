f06c4939bb2330811a51a24708e2fe4b964abb4a
V-Jagath <jagath9360@gmail.com>
2026-01-28 18:21:54 +0530

    crud operation completed

--- Back_End/React/Ailaysa/src/CRUD/Insert.jsx
+++ Back_End/React/Ailaysa/src/CRUD/Insert.jsx
@@ -4,6 +4,10 @@ const Insert = () => {
 
     const [apiData, setApiData] = useState([])
 
+    const [formData, setFormData] = useState([
+        { title: "", price: "", category: "" },
+
+    ])
     useEffect(() => {
         const fetchData = async () => {
             const res = await fetch('https://fakestoreapi.com/products')
@@ -11,12 +15,37 @@ const Insert = () => {
             setApiData(finalRes)
         }
         fetchData()
+
     }, [])
+
+    const getInput = (e) => {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value })
+    }
+
+    const getFormData = (e) => {
+        e.preventDefault()
+
+        const newProduct = { id: apiData.length + 1, ...formData }
+
+        setApiData([...apiData, formData])
+    }
 
     return (
         <>
+            <form onSubmit={getFormData}>
+                <label>Title</label>
+                <input type="text" onChange={getInput} name='title' />
+                <label>Price</label>
+                <input type="text" onChange={getInput} name='price' />
+                <label>Category</label>
+                <input type="text" onChange={getInput} name='category' />
+                <input type="submit" value="Add Product" />
+            </form>
+
             {apiData.map((e) => (
-                <div key={e.id}>
+                <div key={apiData.length + 1}>
+                    <p>id:{e.id}</p>
                     <p>Title : {e.title}</p>
                     <p>Price : {e.price}</p>
                     <p>Category : {e.category}</p>
